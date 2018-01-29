af4f618e75fe62dfd62ef4018dddfe32bbebb717
dmarnol2 <dmarnol2@asu.edu>
2018-01-29 00:34:08 -0500

    uploads folder

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ var app = express();
 var path = require('path');
 var formidable = require('formidable');
 var fs = require('fs');
+//var http = require('http');
+//var url = require('url');
+
 var port = process.env.PORT || 8080;
 
 app.use(express.static(path.join(__dirname, 'public')));
@@ -54,3 +57,19 @@ app.post('/uploads', function(req, res){
 var server = app.listen(port, function(){
   console.log('Server listening on port 8080');
 });
+
+/*
+http.createServer(function (req, res) {
+  var q = url.parse(req.url, true);
+  var filename = "." + q.pathname;
+  fs.readFile(filename, function(err, data) {
+    if (err) {
+      res.writeHead(404, {'Content-Type': 'text/html'});
+      return res.end("404 Not Found");
+    }  
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    return res.end();
+  });
+}).listen(port); //the server object listens on port 8080
+*/
